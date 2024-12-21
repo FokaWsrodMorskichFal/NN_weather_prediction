6bcc63819a1dae96f43bd114ae7c425db53a2a54
FokaWsrodMorskichFal <81437087+FokaWsrodMorskichFal@users.noreply.github.com>
2024-12-21 10:40:12 +0100

    Add files via upload

--- map.py
+++ map.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-
+import numpy as np
+import math
+'''
 # Read data from file
-file_path = '../data/city_attributes.csv'
+file_path = './data/city_attributes.csv'
 data = pd.read_csv(file_path)
 
 # Extract latitude and longitude
@@ -26,3 +28,28 @@ plt.grid(True)
 
 # Show the map
 plt.show()
+'''
+'''
+dropout_rate = 0.3
+
+tab = np.array([1, 2, 3, 4, 3, 2, 1,])
+
+mask = np.random.rand(*tab.shape) > dropout_rate
+
+print(tab)
+print(mask)
+print(tab*mask)
+'''
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+print(arr[0:6])
+
+'''
+days = [i for i in range(365)]
+shifted_days = [((i-135)%365) for i in range(365)]
+print(shifted_days)
+time_angle = [i*2*math.pi/364 for i in shifted_days]
+print(time_angle)
+encoding = [[np.cos(time), np.sin(time)] for time in time_angle]
+print(encoding)
+'''
